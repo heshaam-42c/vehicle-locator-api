2c558f4d8cb9f38d2f01c71b5ab60650272ea8a2
heshaam-42c <heshaam.attar@42crunch.com>
2025-09-26 17:11:23 -0700

    update script

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # This script is used to start the vehicle locator API service.
 
 if [ "$1" == "down" ]; then
-  echo "Stopping Vehicle Locator API Firewall and DB..."
+  echo "Stopping Vehicle Locator API, Firewall and DB..."
 
   # Stop the Database
   cd ./database
@@ -14,8 +14,31 @@ if [ "$1" == "down" ]; then
   ./deployFirewall.sh down
 
   exit 0
+elif [ "$1" == "restart" ]; then
+  echo "Restarting Vehicle Locator API, Firewall and DB..."
+
+  # Stop the Database
+  cd ./database
+  ./database.sh down
+
+  # Stop the API Firewall
+  cd ../api-firewall
+  ./deployFirewall.sh down
+
+  # Start the Database
+  cd ../database
+  ./database.sh
+
+  # Start the API Firewall
+  cd ../api-firewall
+  ./deployFirewall.sh
+
+  # Start the Vehicle Locator API
+  cd ../app
+  npm install -y
+  npm start
 elif [ $# -lt 1 ]; then
-  echo "Starting Vehicle Locator API Firewall and DB..."
+  echo "Starting Vehicle Locator API, Firewall and DB..."
   # Start the Database
   cd ./database
   ./database.sh
